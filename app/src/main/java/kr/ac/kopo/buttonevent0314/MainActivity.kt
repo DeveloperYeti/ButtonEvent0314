e9d68f83535bc6a7193842df64a49f99de51d1f9
DeveloperYeti <hoseok9823@gmail.com>
2024-03-14 16:06:39 +0900

    4개 버튼 처리

--- app/src/main/java/kr/ac/kopo/buttonevent0314/MainActivity.kt
+++ app/src/main/java/kr/ac/kopo/buttonevent0314/MainActivity.kt
@@ -15,8 +15,6 @@ class MainActivity : AppCompatActivity() {
     lateinit var btnFinish: Button
     lateinit var btnUniversity: Button
 
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
